f819b54496f26b9e513b6150e7b822e066a8fbc2
codyreading <codyreading@gmail.com>
2025-06-03 23:46:04 +0100

    Final changes

--- src/data/education.ts
+++ src/data/education.ts
@@ -10,13 +10,13 @@ export interface Education {
 export const educationData: Education[] = [
   // If you don't want to show education, just make the array empty.
   {
-    year: "2019-2021",
+    year: "2019 - 2021",
     institution: "University of Toronto",
     degree: "M.A.Sc. in Aerospace Engineering",
     advisor: "Prof. Steven Waslander",
   },
   {
-    year: "2013—2019",
+    year: "2013 - 2019",
     institution: "University of Waterloo",
     degree: "B.A.Sc. in Mechatronics Engineering",
   },
